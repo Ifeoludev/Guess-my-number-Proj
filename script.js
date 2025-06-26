31638c68c39acf21ae5be17a897d51bf7c4fbd2d
Zikkk <141329565+Ifeoludev@users.noreply.github.com>
2025-06-26 23:02:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -37,27 +37,6 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You lost the game';
       document.querySelector('.score').textContent = 0;
     }
-
-    // } else if (guess > secretNumber) {
-    //   if (score > 1) {
-    //     document.querySelector('.message').textContent = 'Too high!';
-    //     score = score - 1;
-    //     document.querySelector('.score').textContent = score;
-    //   } else {
-    //     document.querySelector('.message').textContent = 'You lost the game';
-    //     document.querySelector('.score').textContent = 0;
-    //   }
-
-    //   //When guess is too low
-    // } else if (guess < secretNumber) {
-    //   if (score > 1) {
-    //     document.querySelector('.message').textContent = 'Too low!';
-    //     score = score - 1;
-    //     document.querySelector('.score').textContent = score;
-    //   } else {
-    //     document.querySelector('.message').textContent = 'You lost the game';
-    //     document.querySelector('.score').textContent = 0;
-    //   }
   }
 });
 
